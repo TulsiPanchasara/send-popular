d6e4294343c90adadbc20796ccaa49b3be1ee00d
tulsipanchasara <panchasaratulsi786@mail.com>
2022-11-20 15:52:25 +0530

    Console added to push updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import type { NextPage } from 'next'
-import WithLayout from '../components/HOC/withLayout'
-import Homepage from '../components/MainComponents/HomePage/Homepage'
+import type { NextPage } from "next";
+import WithLayout from "../components/HOC/withLayout";
+import Homepage from "../components/MainComponents/HomePage/Homepage";
 
 const Home: NextPage = () => {
-  return <Homepage/>
-}
+  console.log("Homepage");
+  return <Homepage />;
+};
 
-export default WithLayout(Home, 'Home')
+export default WithLayout(Home, "Home");
