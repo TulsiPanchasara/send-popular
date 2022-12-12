6b4081c5782c71575768fa81e5b46ae31790d92c
tulsipanchasara <panchasaratulsi786@mail.com>
2022-12-12 21:42:02 +0530

    Constant updated after removal of env

--- config/constants.ts
+++ config/constants.ts
@@ -1,5 +1,5 @@
 export const EMAIL_SERVICE_ID = process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID;
 export const EMAIL_TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID;
 export const EMAIL_USER_ID = process.env.NEXT_PUBLIC_EMAIL_USER_ID;
-export const API_SERVICE_URL = process.env.NEXT_PUBLIC_API_SERVICE_URL;
-export const IMAGE_PATH = process.env.NEXT_PUBLIC_IMAGE_PATH;
+export const API_SERVICE_URL = "https://admin.premrefractories.com/api" // process.env.NEXT_PUBLIC_API_SERVICE_URL;
+export const IMAGE_PATH = "https://admin.premrefractories.com/" // process.env.NEXT_PUBLIC_IMAGE_PATH;
