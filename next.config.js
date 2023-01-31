ed79ba1eee51318c388c9ae945a7f7d63b50c004
abhishek <abhishek@infistack.com>
2023-01-31 23:23:53 +0530

    Install in different OS issue fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false,
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false,path: false, crypto:false };
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
